6a7c5e4d3dc4da56995d61738a13ed8b7b8ddb8e
tanguytasuko <tanguy.cailleaux@isen.yncrea.fr>
2024-03-25 16:01:53 +0100

    post / like / comment v3

--- app/src/main/java/fr/isen/bert/rsspeedrun/MainActivity.kt
+++ app/src/main/java/fr/isen/bert/rsspeedrun/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,6 +21,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.ArrowForward
+import androidx.compose.material.icons.outlined.Send
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -33,7 +35,9 @@ import androidx.compose.ui.unit.dp
 import fr.isen.bert.rsspeedrun.ui.theme.RSSpeedrunTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.LineHeightStyle
 import java.util.*
 
 class MainActivity : ComponentActivity() {
@@ -68,9 +72,15 @@ fun LikeIcon() {
 fun CommentIcon() {
     Icon(imageVector = Icons.Filled.Add, contentDescription = "Comment")
 }
+
+@Composable
+fun SendIcon() {
+    Icon(imageVector = Icons.Outlined.Send, contentDescription = "Send")
+}
+
 @Composable
 fun ResponseIcon() {
-    Icon(imageVector = Icons.Outlined.ArrowForward, contentDescription = "Respond")
+    Icon(imageVector = Icons.Outlined.ArrowForward, contentDescription = "Response")
 }
 
 @Composable
@@ -94,9 +104,11 @@ fun PostView(
     onLike: () -> Unit,
     onCommentAdded: (String) -> Unit,
     modifier: Modifier = Modifier
-){
+) {
     // Utiliser un état local pour gérer le texte du commentaire
     var commentText by remember { mutableStateOf("") }
+    var isCommenting by remember { mutableStateOf(false) } // État local pour gérer la visibilité du champ de commentaire
+    var showComments by remember { mutableStateOf(false) } // État local pour gérer la visibilité des commentaires
 
     Surface(
         modifier = modifier.padding(8.dp),
@@ -111,37 +123,60 @@ fun PostView(
                     LikeIcon()
                 }
                 Spacer(modifier = Modifier.width(8.dp))
-                IconButton(onClick = {
-                    if (commentText.isNotBlank()) {
-                        onCommentAdded(commentText)
-                        commentText = ""
-                    }
-                },) {
+                IconButton(onClick = { isCommenting = !isCommenting }) {
                     CommentIcon()
                 }
             }
-            OutlinedTextField(
-                value = commentText,
-                onValueChange = { commentText = it },
-                label = { Text("Add a comment") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            post.comments.forEach { comment ->
-                CommentView(
-                    comment = comment,
-                    onLike = { /* Ajoutez la logique pour gérer le like ici. Par exemple: */ comment.addLike() },
-                    onRespond = { responseText ->
-                        // Ajoutez la logique pour ajouter une réponse au commentaire ici.
-                        // Cela pourrait ressembler à ajouter un nouveau Comment à la liste des réponses du commentaire.
-                        val newResponse = Comment(id = UUID.randomUUID().toString(), postId = post.id, content = responseText)
-                        comment.addResponse(newResponse)
-                    },
-                    modifier = Modifier.padding(start = 16.dp) // Ajoute une marge à gauche pour les commentaires imbriqués
+            if (isCommenting) { // Affiche le champ de commentaire uniquement lorsque isCommenting est vrai
+                Box {
+                    OutlinedTextField(
+                        value = commentText,
+                        onValueChange = { commentText = it },
+                        label = { Text("Add a comment") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    if (commentText.isNotBlank()) {
+                        IconButton(
+                            onClick = {
+                                onCommentAdded(commentText)
+                                commentText = ""
+                            },
+                            modifier = Modifier.align(Alignment.CenterEnd).padding(end = 8.dp)
+                        ) {
+                            SendIcon()
+                        }
+                    }
+                }
+            }
+            if (post.comments.isNotEmpty()) {
+                Text(
+                    text = if (showComments) "Hide Responses (${post.comments.size})" else "Show Responses (${post.comments.size})",
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.clickable { showComments = !showComments }
                 )
+            }
+            if (showComments) {
+                post.comments.forEach { comment ->
+                    CommentView(
+                        comment = comment,
+                        onLike = { comment.addLike() },
+                        onRespond = { responseText ->
+                            val newResponse = Comment(
+                                id = UUID.randomUUID().toString(),
+                                postId = post.id,
+                                content = responseText
+                            )
+                            comment.addResponse(newResponse)
+                        },
+                        modifier = Modifier.padding(start = 16.dp) // Ajoute une marge à gauche pour les commentaires imbriqués
+                    )
+                }
             }
         }
     }
 }
+
+
 
 @Composable
 fun CommentView(
@@ -151,6 +186,8 @@ fun CommentView(
     modifier: Modifier = Modifier
 ) {
     var responseText by remember { mutableStateOf("") }
+    var isReplying by remember { mutableStateOf(false) } // État local pour gérer la visibilité du champ de réponse
+    var showResponses by remember { mutableStateOf(false) } // État local pour gérer la visibilité des réponses du commentaire
 
     Surface(
         modifier = modifier
@@ -167,38 +204,58 @@ fun CommentView(
                     LikeIcon()
                 }
                 Spacer(modifier = Modifier.width(8.dp))
-                IconButton(onClick = {
-                    if (responseText.isNotBlank()) {
-                        onRespond(responseText)
-                        responseText = ""
-                    }
-                }) {
+                IconButton(onClick = { isReplying = !isReplying }) {
                     ResponseIcon()
                 }
             }
-            OutlinedTextField(
-                value = responseText,
-                onValueChange = { responseText = it },
-                label = { Text("Add a response") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            comment.responses.forEach { response ->
-                // Affichage récursif des réponses comme des commentaires
-                CommentView(
-                    comment = response,
-                    onLike = { /* Ajoutez la logique pour gérer le like ici. Par exemple: */ response.addLike() },
-                    onRespond = { newResponseText ->
-                        // Ajoutez la logique pour ajouter une réponse au commentaire ici.
-                        // Cela pourrait ressembler à ajouter un nouveau Comment à la liste des réponses du commentaire.
-                        val newResponse = Comment(id = UUID.randomUUID().toString(), postId = response.postId, content = newResponseText)
-                        response.addResponse(newResponse)
-                    },
-                    modifier = Modifier.padding(start = 16.dp) // Ajoute une marge à gauche pour les commentaires imbriqués
+            if (isReplying) { // Affiche le champ de réponse uniquement lorsque isReplying est vrai
+                Box {
+                    OutlinedTextField(
+                        value = responseText,
+                        onValueChange = { responseText = it },
+                        label = { Text("Add a response") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    if (responseText.isNotBlank()) {
+                        IconButton(
+                            onClick = {
+                                onRespond(responseText)
+                                responseText = ""
+                            },
+                            modifier = Modifier.align(Alignment.CenterEnd).padding(end = 8.dp)
+                        ) {
+                            SendIcon()
+                        }
+                    }
+                }
+            }
+            if (comment.responses.isNotEmpty()) {
+                Text(
+                    text = if (showResponses) "Hide Responses (${comment.responses.size})" else "Show Responses (${comment.responses.size})",
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.clickable { showResponses = !showResponses }
                 )
+            }
+            if (showResponses) {
+                comment.responses.forEach { response ->
+                    // Affichage récursif des réponses comme des commentaires
+                    CommentView(
+                        comment = response,
+                        onLike = { response.addLike() },
+                        onRespond = { newResponseText ->
+                            val newResponse = Comment(
+                                id = UUID.randomUUID().toString(), postId = comment.postId, content = newResponseText)
+                            response.addResponse(newResponse)
+                        },
+                        modifier = Modifier.padding(start = 16.dp) // Ajoute une marge à gauche pour les commentaires imbriqués
+                    )
+                }
             }
         }
     }
 }
+
+
 
 
 
