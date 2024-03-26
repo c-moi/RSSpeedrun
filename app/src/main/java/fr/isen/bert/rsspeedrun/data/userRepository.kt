a7dd4a665ceef5da88bcd588ee6cf2fbd3106865
Emmanuel <emmanuelbert7@gmail.com>
2024-03-26 14:50:41 +0100

    chang1

---
+++ app/src/main/java/fr/isen/bert/rsspeedrun/data/userRepository.kt
@@ -0,0 +1,53 @@
+package fr.isen.bert.rsspeedrun.data
+
+import com.google.firebase.database.FirebaseDatabase
+
+class userRepository {
+
+    private val database = FirebaseDatabase.getInstance()
+    private val usersRef = database.getReference("users")
+
+    fun readUser() {
+
+    }
+
+    fun addUser(
+        user_id:String = "",
+        first_name:String = "",
+        last_name:String = "",
+        username:String = "",
+        pp:String = "",
+        date2naiss:String = "",
+        email:String = "",
+        psw:String = ""
+    ) {
+
+        val user = User(
+            user_id,
+            first_name,
+            last_name,
+            username,
+            pp,
+            date2naiss,
+            email,
+            psw
+        )
+        usersRef.child(user_id).setValue(user)
+        //.addOnSuccessListener {
+        // L'utilisateur a été ajouté avec succès à la base de données
+        // Vous pouvez effectuer des actions supplémentaires ici si nécessaire
+        //}
+        //.addOnFailureListener { e ->
+        // Une erreur s'est produite lors de l'ajout de l'utilisateur
+        // Vous pouvez gérer l'erreur ici en conséquence
+        //}
+    }
+
+    fun delUser() {
+
+    }
+
+    fun chgUser() {
+
+    }
+}
---
+++ app/src/main/java/fr/isen/bert/rsspeedrun/model/User.kt
@@ -0,0 +1,4 @@
+package fr.isen.bert.rsspeedrun.model
+
+class User {
+}
