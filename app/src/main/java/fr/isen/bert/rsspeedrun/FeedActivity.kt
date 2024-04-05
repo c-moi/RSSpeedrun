dc0f20014ce37167699e79c2f60245aa14096380
theo.pasqualini <theo.pasqualini.2b@gmail.com>
2024-04-05 14:33:02 +0200

    final feed

--- app/src/main/java/fr/isen/bert/rsspeedrun/FeedActivity.kt
+++ app/src/main/java/fr/isen/bert/rsspeedrun/FeedActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,16 +20,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import fr.isen.bert.rsspeedrun.ui.theme.background
+import fr.isen.bert.rsspeedrun.ui.theme.grey
+import fr.isen.bert.rsspeedrun.ui.theme.secondary
 
 class FeedActivity : ComponentActivity() {
     // Liste mutable pour stocker les posts
@@ -180,13 +186,16 @@ fun EditPostContent(
     }
 }
 
+
 @Composable
 fun PostItem(post: List<String>) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp)
-            .background(Color.Green)
+            .clip(shape = RoundedCornerShape(8.dp))
+            .background(grey)
+            .border(5.dp, secondary),
     ) {
         Column(
             modifier = Modifier
@@ -239,7 +248,8 @@ fun PostItem(post: List<String>) {
                 Button(
                     onClick = {
                         // Action lorsque le bouton "Like" est cliqué
-                    }
+                    },
+                    colors = ButtonDefaults.buttonColors(background)
                 ) {
                     Text("Like")
                 }
@@ -248,7 +258,8 @@ fun PostItem(post: List<String>) {
                 Button(
                     onClick = {
                         // Action lorsque le bouton "Comment" est cliqué
-                    }
+                    },
+                    colors = ButtonDefaults.buttonColors(Color.Gray)
                 ) {
                     Text("Comment")
                 }
@@ -256,6 +267,7 @@ fun PostItem(post: List<String>) {
         }
     }
 }
+
 
 @Composable
 fun DisplayPosts(postsList: List<List<String>>) {
